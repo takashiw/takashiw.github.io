4b89a3feee6f7a9a261b09e39b96328cd21470c2
iTwicKz <twickes32@ufl.edu>
2017-11-15 10:38:40 -0500

    Removed hash from url

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
-import {HashRouter} from 'react-router-dom'
+import {BrowserRouter} from 'react-router-dom'
 import registerServiceWorker from './registerServiceWorker'
 
 ReactDOM.render(
-  <HashRouter>
+  <BrowserRouter>
     <App />
-  </HashRouter>,
+  </BrowserRouter>,
   document.getElementById('root')
 )
 
---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import Home from './components/Home/Home'
+import Theia from './components/ProjectDetailPage/project-Theia'
+import DetailsMyopic from './components/ProjectDetailPage/project-Myopic'
+import ProjectMedidock from './components/ProjectDetailPage/project-Medidock'
+import ProjectMango from './components/ProjectDetailPage/project-Mango'
+
+export default (
+
+)
